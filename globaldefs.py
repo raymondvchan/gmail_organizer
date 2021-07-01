8d91d1b1ca11e477ac68c38c83296e887b2c7dc0
raymondvchan <39813003+raymondvchan@users.noreply.github.com>
2021-07-01 09:38:01 -0700

    Update globaldefs.py

--- globaldefs.py
+++ globaldefs.py
@@ -1,2 +1,2 @@
-SECRET_KEY = '''''' # Google API KEY
+SECRET_KEY = '''GoogleAPIKey''' # Google API KEY
 DB_PROPERTIES = '''sqlite:///properties.db''' # sqlalchemy connection... sqlite, postgres, mysql etc..
